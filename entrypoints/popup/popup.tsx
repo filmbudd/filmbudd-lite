4941466972bb7b675bc62eb4cc215116e58fd8e2
kakuiho <61496907+kakuiho@users.noreply.github.com>
2024-07-31 00:27:15 +0800

    popup doesn't use module CSS

--- entrypoints/popup/popup.tsx
+++ entrypoints/popup/popup.tsx
@@ -3,21 +3,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import * as configs from "@/src/configs";
-import classes from "./popup.module.css";
+import "./popup.css";
 
 function App() {
   const manifest = browser.runtime.getManifest();
   const _ = browser.i18n.getMessage;
 
   return (
-    <div className={classes.container}>
-      <h1 className={classes.h1}>{manifest.name}</h1>
+    <div className="container">
+      <h1 className="h1">{manifest.name}</h1>
       <p>
-        <a target="_blank" href={configs.URL_FEEDBACK} className={classes.a}>
+        <a target="_blank" href={configs.URL_FEEDBACK} className="a">
           {_("Feedback")}
         </a>
       </p>
-      <p className={classes.version}>v{manifest.version}</p>
+      <p className="version">v{manifest.version}</p>
     </div>
   );
 }
