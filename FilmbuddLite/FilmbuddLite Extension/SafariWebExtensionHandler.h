db0e35d03fc88c815f8e58f3bf162d9ca5456625
kakuiho <61496907+kakuiho@users.noreply.github.com>
2024-12-10 18:12:41 +0800

    Added xcode project files.

---
+++ FilmbuddLite/FilmbuddLite Extension/SafariWebExtensionHandler.h
@@ -0,0 +1,9 @@
+//
+// Copyright © 2024 Filmbudd LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SafariWebExtensionHandler : NSObject <NSExtensionRequestHandling>
+
+@end
